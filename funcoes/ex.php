32541f35de5f20a0b7363d0682fba2c0088a8536
Rodrigo Prieto <rodrigo.prieto@gosoftwares.com.br>
2018-05-15 16:27:17 -0300

    ok

--- funcoes/ex.php
+++ funcoes/ex.php
@@ -1,7 +1,23 @@
 <?php
 function ola()
 {
-  return "Olá Mundo!";
+    $teste = 1;
+    return "Olá Mundo!";
+    $teste = 2;
 }
+
+function olaPar($texto = "Valor Padrão")
+{
+    return "<br/>Olá " . $texto;
+}
+
 echo ola();
- ?>
+$frase = ola();
+echo "<br>" . strlen($frase);
+
+echo olaPar("Rodrigo<br>");
+var_dump($frase);
+//echo  phpinfo();
+
+
+?>
